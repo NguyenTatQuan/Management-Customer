6163c03a5e4835f2646860bc040eb04a1660df13
NguyenTatQuan <tatquan2001@gmail.com>
2024-09-25 17:21:05 +0700

    up FE

---
+++ Management-Customer-FE/login/login.js
@@ -0,0 +1,21 @@
+function login(){
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    let u ={
+        "username": username,
+        "password": password
+    }
+    $.ajax({
+        headers:{
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        },
+        method: "POST",
+        data: JSON.stringify(u),
+        url: "http://localhost:8080/api/auth/login",
+        success: function (dulieu) {
+            localStorage.setItem("u", JSON.stringify(dulieu));
+            window.location.href = "../customer/list.html"
+        }
+    })
+}
