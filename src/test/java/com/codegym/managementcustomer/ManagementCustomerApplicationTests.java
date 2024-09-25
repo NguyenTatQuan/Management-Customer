51bc662ead2d84f6be846c937f6e5f144cd275e1
NguyenTatQuan <tatquan2001@gmail.com>
2024-09-25 14:59:45 +0700

    up

---
+++ src/test/java/com/codegym/managementcustomer/ManagementCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codegym.managementcustomer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementCustomerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
